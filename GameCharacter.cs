2996a8e91fd83f968547f6457486fd1ef7bb698d
imsern <kristofferims@hotmail.com>
2022-01-28 16:46:44 +0100

    Add project files.

---
+++ GameCharacter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bossfight
+{
+    internal class GameCharacter
+    {
+        public Random random = new();
+
+        public string Player;
+        public int Health;
+        public int Strength;
+        public int Stamina;
+        public int Maxstamina;
+
+        public GameCharacter(string player, int health, int str, int stam)
+        {
+            Player = player;
+            Health = health;
+            Strength = str;
+            Stamina = stam;
+            Maxstamina = stam;
+        }
+
+        public void Fight(GameCharacter Opponent)
+        {
+            if (Stamina <= 0)
+            {
+                Recharge();
+            }
+            else if (Opponent.Player == "Hero" && Stamina >= 10)
+            {
+                Strength = random.Next(0, 30);
+                Opponent.Health -= Strength;
+                Stamina -= 10;
+
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine($"{Player} hit {Opponent.Player} for {Strength}! {Opponent.Player} has {Opponent.Health} health left ({Stamina}/{Maxstamina} Stamina)");
+            }
+            else if (Opponent.Player == "Boss" && Stamina >= 10)
+            {
+                Opponent.Health -= Strength;
+                Stamina -= 10;
+
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine($"{Player} hit {Opponent.Player} for {Strength}! {Opponent.Player} has {Opponent.Health} health left ({Stamina}/{Maxstamina} Stamina)");
+            }
+        }
+
+        private void Recharge()
+        {
+            Stamina = Maxstamina;
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine($"{Player} had to restore Stamina!");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿
+using Bossfight;
+
+var player1 = new GameCharacter("Hero", 100, 20, 40);
+var player2 = new GameCharacter("Boss", 400, 30, 10);
+
+var Alive = true;
+while (Alive)
+{
+    player1.Fight(player2);
+    await Task.Delay(1000);
+    player2.Fight(player1);
+    if (player1.Health <= 0 || player2.Health <= 0)
+    {
+        Alive = false;
+        if (player1.Health <= 0)
+        {
+            Console.WriteLine($"{player2.Player} wins!");
+        } else if(player2.Health <= 0) Console.WriteLine($"{player1.Player} wins!");
+
+    }
+    await Task.Delay(1000);
+
+}
